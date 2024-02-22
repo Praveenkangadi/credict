b5ce68ff86508a9f135f456464d1918a44758295
Praveen Kumar <praveenkumarpk8861@gmail.com>
2024-02-22 13:31:46 +0530

    new

--- app.py
+++ app.py
@@ -10,16 +10,17 @@ def home():
   r= ''
   if request.method=='POST':
     data = [request.form['a'], request.form['b'], request.form['c'], request.form['d'], request.form['e'], request.form['f'], request.form['g'], request.form['h'], request.form['i'], request.form['j'], request.form['k'], request.form['l']] 
-    data2 = [int(i) for i in data]
+    data2 = [float(i) for i in data]
 
     res = getResult(data2)
     if res==0:
-      r = "credit score:Good"
+      r = "Good"
     elif res==1:
-      r ="credit score: poor"
+      r ="Poor"
     else:
-      r = "credit score: standard"
+      r = "Standard"
   return render_template('index.html', result=r)
 
 if __name__=='__main__':
+  # app.run(debug=True)
   serve(app, host='0.0.0.0', port=50100)
--- logic/algorithm.py
+++ logic/algorithm.py
@@ -1,12 +1,12 @@
 import os
 import joblib
 import numpy as np
-logisticRegression = joblib.load(os.getcwd()+r'/logic/RandomForest.joblib')
+RandomForest = joblib.load(os.getcwd()+r'/logic/RandomForest.joblib')
 
 def getResult(li:list):
 
   features = np.array([li])
 
-  pred = logisticRegression.predict(features)
+  pred = RandomForest.predict(features)
 
   return pred[0]
